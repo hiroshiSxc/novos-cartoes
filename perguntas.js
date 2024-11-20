1dd48eb86ceb0f1aaa0a9fcf365115d0c747f1cd
hiroshiSxc <141240361+hiroshiSxc@users.noreply.github.com>
2024-11-20 12:34:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Esporte',
+    'Quem é o jogador com mais gols marcados pelo Real Madrid?',
+    'Cristiano Ronaldo(vulgo papai cris).'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantos títulos da La Liga o Real Madrid conquistou até 2024?',
+    '35 título'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem foi o técnico do Real Madrid que levou o time à vitória na Liga dos Campeões em 2016, 2017 e 2018?',
+    'Zinedine Zidane.'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual ano foi fundado o Real Madrid?',
+    '1902'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantas Ligas dos Campeões o Real Madrid venceu até o momento?',
+    '14 Ligas dos Campeões da UEFA'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem é o principal rival do Real Madrid?',
+    'Resposta: FC Barcelona (El Clásico).'
+)
+
+criaCartao(
+    'Esporte',
+    'Em que ano o Real Madrid conquistou seu primeiro título da Liga dos Campeões?',
+    ' 1956.'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem foi o fundador do Real Madrid?',
+    'Santiago Bernabéu Yeste.'
+)
